d3156d132b3433e25a8c19703f73f01b5797b36b
ngalela01 <149369383+ngalela01@users.noreply.github.com>
2025-07-07 14:29:43 +0200

    la maison-jungle

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Banner from './Banner.js';
+import Cart from './Cart.js';
+
+function App() {
+  return (
+    <div>
+    <Banner/>
+    <Cart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import '../assets/styles/Banner.css'
+import logo from '../assets/logo.png'
+function Banner() {
+    return (
+     <div className="lmj-banner">
+        <h1>La maison jungle</h1>
+     </div>
+    )
+}
+
+export default Banner
