3a604e1cda71bfad35a7bf9b9270e2d5cf4e993f
ngalela01 <149369383+ngalela01@users.noreply.github.com>
2025-07-08 10:24:43 +0200

    affichage des categories de plantes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,11 @@
 import '../assets/styles/Banner.css'
 import logo from '../assets/logo.png'
 function Banner() {
+    const title="la maison jungle"
     return (
      <div className="lmj-banner">
-        <h1>La maison jungle</h1>
+        <img src="../assets/styles/logo.png" alt="image-logo la maison-jungle" />
+        <h1>{title}</h1>
      </div>
     )
 }
